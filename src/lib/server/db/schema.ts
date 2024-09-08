d5460039705f813bbc5f8623ebaf6e1c55df2190
mehrad <mmfirst@protonmail.com>
2024-09-08 20:14:27 +0330

    Databse with drizzle and sqlite-better

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,13 @@
+import type { InferInsertModel } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const post = sqliteTable("post", {
+    id: integer("id").primaryKey(),
+    name: text("name").notNull(),
+    author: text("author").notNull(),
+    slug: text("slug").notNull(),
+    description: text("description").notNull(),
+    markdown: text("markdown").notNull(),
+});
+
+export type Post = InferInsertModel<typeof post>;
