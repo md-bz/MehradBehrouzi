85f43f446125fc058002e055d50ef41734d58397
mehrad <mmfirst@protonmail.com>
2024-09-08 18:21:44 +0330

    scaffolding for project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Bloggy</a>
+                </li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog">blog</a></li>
+            </ul>
+
+            <ul>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+
+                <li>
+                    <a href="/signup">Signup</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <slot></slot>
+    </main>
+</body>
---
+++ src/routes/blog/[slug]/+server.ts
@@ -0,0 +1,4 @@
+export function load({ params }: { params: { slug: string } }) {
+    console.log(params.slug);
+    return {};
+}
