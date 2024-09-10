6518fa28ac645672f16712e0d995e58a0d158953
mehrad <mmfirst@protonmail.com>
2024-09-10 13:21:59 +0330

    Implemented author logic with jwt and API so only logged in users can post

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -1,8 +1,16 @@
+import { env } from "$env/dynamic/private";
 import { db } from "$lib/server/db";
 import { post } from "$lib/server/db/schema";
 import { fail, type Cookies } from "@sveltejs/kit";
 import { writeFile } from "fs/promises";
 import markdownIt from "markdown-it";
+
+async function getMe(jwt: string) {
+    const res = await fetch(`${env.API_URL}/users/me`, {
+        headers: { Authorization: `Bearer ${jwt}` },
+    });
+    return await res.json();
+}
 
 const maxSize = 5; //mb
 
@@ -14,6 +22,9 @@ export const actions = {
         cookies: Cookies;
         request: Request;
     }) => {
+        const jwt = cookies.get("jwt");
+        if (!jwt) return fail(401, { unauthorized: true });
+
         const data = await request.formData();
 
         const name = data.get("name");
@@ -23,13 +34,16 @@ export const actions = {
         if (!file || !name || !description) return fail(422, { missing: true });
         file = file as File;
 
+        const res = await getMe(jwt);
+        if (res.status == "fail") return fail(401, { unauthorized: true });
+        const author = res.data.document.name;
+
         if (file.type !== "text/markdown")
             return fail(422, { wrongType: true });
 
         if (file.size > maxSize * 1000 * 1000)
             return fail(422, { tooLarge: true });
 
-        const author = "placeHolder";
         const slug = `${author}-${name}`;
 
         await db.insert(post).values({
