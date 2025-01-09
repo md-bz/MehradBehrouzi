616e1bacbf67c940f7fcdf6efc87cf72441c6d48
mehrad <mmfirst@protonmail.com>
2025-01-09 19:38:04 +0330

    replaced telegraf with simple fetch for simpilcity

--- src/lib/server/telegram.ts
+++ src/lib/server/telegram.ts
@@ -1,11 +1,9 @@
 import { env } from "$env/dynamic/private";
-import { Telegraf } from "telegraf";
 
 export async function sendToTelegram(markdown: string) {
-    const bot = new Telegraf(env.BOT_TOKEN);
-    bot.launch();
-    await bot.telegram.sendMessage(env.TELEGRAM_ID, markdown, {
-        parse_mode: "MarkdownV2",
-    });
-    bot.stop();
+    await fetch(
+        `https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage?chat_id=${
+            env.TELEGRAM_ID
+        }&text=${encodeURIComponent(markdown)}&parse_mode=MarkdownV2`
+    );
 }
